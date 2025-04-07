872612a82c0c7462469c42aca94d77635e368271
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-04-07 22:53:15 +0100

    fix import path; correct dotenv initialization path in import.php

--- backend/src/Config/import.php
+++ backend/src/Config/import.php
@@ -1,6 +1,7 @@
 <?php
+require_once __DIR__ . '/../../vendor/autoload.php'; 
 // Database configuration
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
 $host = $_ENV['DB_HOST'];
