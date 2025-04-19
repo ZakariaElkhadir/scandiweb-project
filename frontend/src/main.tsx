7b2f8dbb2aac36f68c64b48c281ec6e2058bf359
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-04-19 13:40:42 +0100

    Update Apollo Client URI to use environment variables for backend URL

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 const client = new ApolloClient({
-  uri: "/graphql",
+  uri: import.meta.env.DEV
+    ? "/graphql"
+    : `${import.meta.env.VITE_BACKEND_URL}/graphql`,
   cache: new InMemoryCache(),
 });
 createRoot(document.getElementById("root")!).render(
