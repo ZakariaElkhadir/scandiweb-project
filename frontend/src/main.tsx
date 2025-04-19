f0f1cf990c9a4f6cb5f940e41acaec8ed55001a2
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-04-19 20:39:34 +0100

    Refactor Apollo Client setup to remove createHttpLink and simplify URI handling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,19 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client";
-const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_BACKEND_URL 
-    ? `${import.meta.env.VITE_BACKEND_URL}/graphql` 
-    : "/graphql",
-  credentials: 'include',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 const client = new ApolloClient({
-  link: httpLink,
+  uri: import.meta.env.DEV
+    ? "/graphql"
+    : `${import.meta.env.VITE_BACKEND_URL}`,
   cache: new InMemoryCache(),
 });
 createRoot(document.getElementById("root")!).render(
