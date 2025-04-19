b8a32fe6ae70fb0acc50cc855ebcd205c84d7448
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-04-19 15:41:29 +0100

    Refactor Apollo Client setup to use createHttpLink for improved backend URL handling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client";
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_BACKEND_URL 
+    ? `${import.meta.env.VITE_BACKEND_URL}/graphql` 
+    : "/graphql",
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 const client = new ApolloClient({
-  uri: import.meta.env.DEV
-    ? "/graphql"
-    : `${import.meta.env.VITE_BACKEND_URL}`,
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 createRoot(document.getElementById("root")!).render(
