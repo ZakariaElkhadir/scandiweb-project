cbe22e87fff4a7fdfb2b46c280ddf6d9820d67a3
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-04-19 12:55:46 +0100

    Add .env to gitignore and use environment variables in proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/graphql/, '/graphql'),
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig(({ mode }) => {
+  // Load .env variables
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/graphql": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/graphql/, "/graphql"),
+        },
       },
     },
-  }
-})
+  };
+});
